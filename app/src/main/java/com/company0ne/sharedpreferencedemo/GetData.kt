91d56f96e6c0ae5da40cafa5bf21b981b3117fce
anandrex5 <anandrex5@gmail.com>
2023-08-03 16:49:59 +0530

    First commit

---
+++ app/src/main/java/com/company0ne/sharedpreferencedemo/GetData.kt
@@ -0,0 +1,33 @@
+package com.company0ne.sharedpreferencedemo
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class GetData:AppCompatActivity() {
+    //Declare the View where we want to update the user entered data
+     private lateinit var nameOutput: TextView
+     private lateinit var ageOutput: TextView
+
+     override fun onCreate(savedInstanceState: Bundle?) {
+          super.onCreate(savedInstanceState)
+          setContentView(R.layout.activity_get_data)
+
+         //find the id of the views
+          nameOutput = findViewById(R.id.getName)
+          ageOutput = findViewById(R.id.getAge)
+     }
+    //we use resume method
+      override fun onResume() {
+               super.onResume()
+
+               //and get Data inside it
+               val sharedPreferences = getSharedPreferences("MySharedPref", MODE_PRIVATE)
+               val getName = sharedPreferences.getString("name", "")
+               val getAage = sharedPreferences.getString("age", "")
+
+                //update our textView from the user entered data
+                nameOutput.text = getName
+                ageOutput.text = getAage
+          }
+     }
---
+++ app/src/main/java/com/company0ne/sharedpreferencedemo/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.company0ne.sharedpreferencedemo
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+
+class MainActivity : AppCompatActivity() {
+
+    //Declare the all views
+    private lateinit var age: TextView
+    private lateinit var name: TextView
+    private lateinit var button: Button
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //find the all view
+        name = findViewById(R.id.edit1)
+        age = findViewById(R.id.edit2)
+        button = findViewById(R.id.button)
+
+        // Load the saved data from SharedPreferences or entered by the User
+        val sharedPreferences = getSharedPreferences("MySharedPref", MODE_PRIVATE)
+        name.setText(sharedPreferences.getString("name", ""))
+        age.setText(sharedPreferences.getString("age", ""))
+        //if u remove only this block(Line 27-31), the user stored data is not displayed in EditText
+
+
+
+        //set onClickListener
+        button.setOnClickListener {
+
+            val sharedPreferences = getSharedPreferences("MySharedPref", MODE_PRIVATE)
+            val editor = sharedPreferences.edit()
+            // write all the data entered by the user in SharedPreference and apply
+            editor.putString("name", name.text.toString())
+            editor.putString("age", age.text.toString())
+            editor.apply()
+
+            // Start-Pass Data to GetData activity
+            val intent = Intent(this@MainActivity, GetData::class.java)
+            startActivity(intent)
+        }
+    }
+}
